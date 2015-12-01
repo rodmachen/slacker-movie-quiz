22319a0a881b49a9e1254da95623d20ec285d79f
Rod Machen <mail@rodmachen.com>
2015-11-30 19:37:01 -0600

    Add middleware requires and settings

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var mongodb = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var validator = require('express-validator');
 
 if (process.env.PORT) { // if running on Heroku
   app.set('port', (process.env.PORT));
@@ -17,6 +19,10 @@ app.use(express.static(__dirname + '/public'));
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+app.use(bodyParser.json());
+app.use(validator([])); // this line must be immediately after express.bodyParser()!
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // app.get('/', function(request, response) {
 //   // TODO add default get request
