32de240425428892379cd30e713ec0c47a49ea82
Rod Machen <mail@rodmachen.com>
2015-12-01 16:55:39 -0600

    Implement sending results by email

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var validator = require('express-validator');
 var cors = require('cors');
+var nodemailer = require('nodemailer');
 
 if (process.env.PORT) { // if running on Heroku
   app.set('port', (process.env.PORT));
   mongoose.connect(process.env.MONGOLAB_URI);
+  senderEmail = process.env.SENDER_EMAIL;
+  senderPassword = process.env.SENDER_PASSWORD;
 } else { // running locally
   app.set('port', (5000));
   var local = require('./config.js');
   mongoose.connect(local.MONGOLAB_URI);
+  senderEmail = local.SENDER_EMAIL;
+  senderPassword = local.SENDER_PASSWORD;
 }
 
 app.use(express.static(__dirname + '/public'));
@@ -30,7 +35,35 @@ app.options('*', cors());
 
 app.post('/', function(req, res) {
   console.log('posted');
+  sendMail(req.body.email, req.body.score);
 });
+
+function sendMail(address, score) {
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: senderEmail,
+      pass: senderPassword,
+    },
+  });
+
+  var mailOptions = {
+    from: senderEmail, // sender address
+    to: address, // list of receivers
+    subject: 'Your Results from The Slacker Quiz!', // Subject line
+    text: 'You received a score of ' + score + '% on The Slacker Quiz!', // plaintext body
+    html: '<b>You received a score of ' + score + '% on The Slacker Quiz!</b>', // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log('Message sent: ' + info.response);
+  });
+}
 
 // app.get('/', function(request, response) {
 //   // TODO add default get request
