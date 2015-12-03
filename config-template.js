be1e18f801879b9fa31c3d3792f7472dd7ef37ec
Rod Machen <mail@rodmachen.com>
2015-12-03 10:53:12 -0600

    Add config template

---
+++ config-template.js
@@ -0,0 +1,8 @@
+// fill in the following values
+// change this file name to config.js
+// config.js is in .gitignore for security
+module.exports = {
+  MONGOLAB_URI: 'mongodb://your-mongodb-address', // Mongolab is a great option. Available through Heroku.
+  SENDER_EMAIL: 'sender@email.address',
+  SENDER_PASSWORD: 'senderpassword',
+};
