c82133ac7fa3ac418a9babfa078a64fc05088f25
Rod Machen <mail@rodmachen.com>
2015-12-02 19:48:57 -0600

    Code cleanup

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Required modules
 var express = require('express');
 var app = express();
 var mongodb = require('mongodb');
@@ -8,12 +9,15 @@ var validator = require('express-validator');
 var cors = require('cors');
 var nodemailer = require('nodemailer');
 
-if (process.env.PORT) { // if running on Heroku
+// Set variables for production or development environment
+// If running on Heroku
+if (process.env.PORT) {
   app.set('port', (process.env.PORT));
   mongoose.connect(process.env.MONGOLAB_URI);
   senderEmail = process.env.SENDER_EMAIL;
   senderPassword = process.env.SENDER_PASSWORD;
-} else { // running locally
+// If running locally
+} else {
   app.set('port', (5000));
   var local = require('./config.js');
   mongoose.connect(local.MONGOLAB_URI);
@@ -21,39 +25,45 @@ if (process.env.PORT) { // if running on Heroku
   senderPassword = local.SENDER_PASSWORD;
 }
 
+// Set static serving and fire up server
 app.use(express.static(__dirname + '/public'));
-
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+// Middleware
 app.use(bodyParser.json());
 app.use(validator([])); // this line must be immediately after express.bodyParser()!
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Handle cross-site scripting blockage
 app.use(cors());
 app.options('*', cors());
 
+// Start the database
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(callback) {
   console.log('once opened');
 });
 
+// Define database schema
 var quizSchema = mongoose.Schema({
   email: String,
   score: Number,
 });
 
+// Define database model
 var Quizee = mongoose.model('Quizee', quizSchema);
 
+// POST route with database entry and results emailed
 app.post('/', function(req, res) {
-  // TODO correct status code, and registers
   console.log('posted');
   Quizee.create(req.body);
   sendMail(req.body.email, req.body.score);
   res.sendStatus(200);
 });
 
+// Sends results by email using Nodemailer
 function sendMail(address, score) {
   var transporter = nodemailer.createTransport({
     service: 'Gmail',
@@ -71,7 +81,6 @@ function sendMail(address, score) {
     html: '<b>You received a score of ' + score + '% on The Slacker Quiz!</b>', // html body
   };
 
-  // send mail with defined transport object
   transporter.sendMail(mailOptions, function(error, info) {
     if (error) {
       return console.log(error);
@@ -80,7 +89,3 @@ function sendMail(address, score) {
     console.log('Message sent: ' + info.response);
   });
 }
-
-// app.get('/', function(request, response) {
-//   // TODO add default get request
-// });
