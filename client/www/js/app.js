d0ce9f263315d9999dbf14a6a8c549b9b0f9deb6
Rod Machen <mail@rodmachen.com>
2015-12-09 21:52:03 -0600

    Refactor to implement multi-page quiz

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -18,24 +18,20 @@ angular.module('quizApp', ['ionic'])
 // Home Controller
 .controller('HomeCtrl', function($scope, data, $state, $http) {
 
-  // User object for storing user data
   $scope.user = data;
 
-  // 1st View Submit Button
-  // Captures email and moves to 2nd View
   $scope.submitEmail = function() {
     if (this.email) {
       $scope.user.email = this.email;
       console.log($scope.user);
-      $state.go('^.' + 'quiz');
+      $state.go('^.' + 'quiz1');
     }
   };
 
 })
 
-
 // Quiz Controller
-.controller('QuizCtrl', function($scope, data, $state, $http) {
+.controller('QuizCtrl', function($scope, data, results, $state, $http) {
 
   // List of the quiz questions
   $scope.quizList = [
@@ -43,13 +39,14 @@ angular.module('quizApp', ['ionic'])
     { text: 'The Continental Club', checked: false },
     { text: 'GM Steakhouse', checked: false },
     { text: 'Quack\'s on the Drag', checked: false },
-    { text: 'Half Price Books', checked: false },
   ];
+  $scope.user = data;
+  $scope.results = results;
 
-  // Array for keeping track of selected answers
+  // Array for keeping track of current answer
   $scope.selection = [];
 
-  // Keeps track of selected answers
+  // Keeps track of current answer
   $scope.toggleSelection = function toggleSelection(question) {
     var idx = $scope.selection.indexOf(question);
 
@@ -65,13 +62,37 @@ angular.module('quizApp', ['ionic'])
 
   };
 
-  // User object for storing user data
-  $scope.user = data;
+  function recordAnswer() {
+    if ($scope.selection.length > 0) {
+      $scope.results.correct++;
+    }
 
-  // 2nd View Submit Button
-  // Captures score and moves to 3rd View
+    $scope.results.questions++;
+    $scope.selection = [];
+  }
+
+  $scope.goQuiz2 = function() {
+    recordAnswer();
+    $scope.quizList[0].checked = false;
+    $state.go('^.' + 'quiz2');
+  };
+
+  $scope.goQuiz3 = function() {
+    recordAnswer();
+    $scope.quizList[1].checked = false;
+    $state.go('^.' + 'quiz3');
+  };
+
+  $scope.goQuiz4 = function() {
+    recordAnswer();
+    $scope.quizList[2].checked = false;
+    $state.go('^.' + 'quiz4');
+  };
+
   $scope.submitQuiz = function() {
-    $scope.user.score = $scope.selection.length / $scope.quizList.length * 100;
+    recordAnswer();
+    $scope.quizList[3].checked = false;
+    $scope.user.score = $scope.results.correct / $scope.results.questions * 100;
     console.log($scope.user);
     $http.post('https://slacker-server.herokuapp.com/', $scope.user)
       .then(function() {
@@ -85,16 +106,14 @@ angular.module('quizApp', ['ionic'])
 })
 
 // Results Controller
-.controller('ResultsCtrl', function($scope, data, $state, $http) {
+.controller('ResultsCtrl', function($scope, data, results, $state, $http) {
 
-  // User object for storing user data
   $scope.user = data;
-
-  // 3rd View Submit Button
-  // Empties User object and moves back to 1st View
+  $scope.results = results;
   $scope.submitRestart = function() {
     $scope.user = {};
-    console.log($scope.user);
+    $scope.results.questions = 0;
+    $scope.results.correct = 0;
     $state.go('^.' + 'home');
   };
 
@@ -104,25 +123,44 @@ angular.module('quizApp', ['ionic'])
 .config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
 
-  // 1st View
   $stateProvider.state('home', {
     url: '/',
     templateUrl: 'views/home.html',
   });
 
-  // 2nd View
-  $stateProvider.state('quiz', {
-    url: '/quiz',
-    templateUrl: 'views/quiz.html',
+  $stateProvider.state('quiz1', {
+    url: '/quiz1',
+    templateUrl: 'views/quiz1.html',
   });
 
-  // 3rd View
+  $stateProvider.state('quiz2', {
+    url: '/quiz2',
+    templateUrl: 'views/quiz2.html',
+  });
+
+  $stateProvider.state('quiz3', {
+    url: '/quiz3',
+    templateUrl: 'views/quiz3.html',
+  });
+
+  $stateProvider.state('quiz4', {
+    url: '/quiz4',
+    templateUrl: 'views/quiz4.html',
+  });
+
   $stateProvider.state('results', {
     url: '/results',
     templateUrl: 'views/results.html',
   });
 })
 
-.factory('data', function () {
-    return {};
+.factory('data', function() {
+  return {};
+})
+
+.factory('results', function() {
+  return {
+    questions: 0,
+    correct: 0,
+  };
 });
